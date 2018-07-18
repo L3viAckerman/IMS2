be56998e77fed219c7e61d1311d7bc83512df3c4
AnhDH <anhdhbn@gmail.com>
2018-07-18 14:58:50 +0700

    add file cmd

---
+++ IMSv2/UpdateModelNoBuild.cmd
@@ -0,0 +1,2 @@
+dotnet ef dbcontext scaffold  "data source=den1.mssql5.gear.host;initial catalog=IMS;persist security info=True;user id=imsv2;password=Qy1?~4kE16yY;multipleactiveresultsets=True;" Microsoft.EntityFrameworkCore.SqlServer  -o Models -f --no-build 
+
