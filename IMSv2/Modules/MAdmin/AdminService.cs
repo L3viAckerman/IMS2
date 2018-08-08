3fbf9fa803de12a76d5c812a5915e5cb1860d46b
L3viAckernam <lephamvanlinh@gmail.com>
2018-08-08 15:54:13 +0700

    demo Modul Admin

---
+++ IMSv2/Modules/MAdmin/AdminService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMSv2.Modules.MAdmin
+{
+    public class AdminService
+    {
+    }
+}
